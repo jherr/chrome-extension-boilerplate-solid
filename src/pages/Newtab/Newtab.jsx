9e67c783658991e9c9d74d9ff80c779ee95529e8
Jack Herrington <jherr@pobox.com>
2022-01-10 17:44:15 -0800

    Removing React logo

--- src/pages/Newtab/Newtab.jsx
+++ src/pages/Newtab/Newtab.jsx
@@ -1,4 +1,3 @@
-import logo from '../../assets/img/logo.svg';
 import './Newtab.css';
 import './Newtab.scss';
 
@@ -6,7 +5,6 @@ const Newtab = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/pages/Newtab/Newtab.js</code> and save to reload.
         </p>
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,4 +1,3 @@
-import logo from '../../assets/img/logo.svg';
 import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
 
@@ -6,7 +5,6 @@ const Popup = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/pages/Popup/Popup.jsx</code> and save cool reload.
         </p>
